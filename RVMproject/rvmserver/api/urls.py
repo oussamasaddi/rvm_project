dc24502438348b59d3e84f411e01a24b28ba86a2
ATJ12 <62201231+ATJ12@users.noreply.github.com>
2023-04-22 02:28:20 +0200

    :penguin:

--- RVMproject/rvmserver/api/urls.py
+++ RVMproject/rvmserver/api/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path 
 from . import views 
+from . import machine 
+from . import bottles 
 
 urlpatterns = [
     path('',views.getAllusers),
-    path('add/',views.addUser)
+    path('add/',views.addUser),
+    path('getAllmachines/',machine.getAllmachines),
+    path('addmachine/',machine.addMachine),
+    path('<str:id>/deleteMachine/',machine.deleteMachine),
+    path('<str:id>/updateMachine/',machine.updateMachine),
+    path('searchM/', machine.searchMachines),
+    path('deleteAllMachines/', machine.deleteAllMachines),
+    path('addbottles/', bottles.addbottles),
+    path('getAllbottles/', bottles.getAllbottles),
+    path('<str:id>/deletebottle/',bottles.deletebottle),
+    path('<str:id>/updateBottle/',bottles.updateBottle),
+    path('search_B/',bottles.searchbottles),
+    path('deleteAllbottles/', bottles.deleteAllMachines),
+
+
+
+
+
+
+
 ]
