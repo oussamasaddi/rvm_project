c58f77d948cb96a231ae67728409d5b751b1c65e
oussamasaddi <83046047+oussamasaddi@users.noreply.github.com>
2023-04-24 22:51:08 +0100

    ALLOWED_HOSTS = ['.vercel.app']
    zidt .vercel.app

--- RVMproject/rvmserver/rvmserver/settings.py
+++ RVMproject/rvmserver/rvmserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mm7tjw0awaezjz-sbravm7)0igpd@&(5q$_p^gpd4t-o4kil8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
